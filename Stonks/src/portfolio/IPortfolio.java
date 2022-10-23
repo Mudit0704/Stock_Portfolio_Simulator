b946ae548cd9f2ead5d816fd3f60a0211956eea4
Mudit0704 <maheshwari7mudit@gmail.com>
2022-10-23 14:31:11 -0400

    Adding portfolio interface.

--- Stonks/src/portfolio/IPortfolio.java
+++ Stonks/src/portfolio/IPortfolio.java
@@ -1,5 +1,13 @@
 package portfolio;
 
-public interface IPortfolio {
+import java.io.FileNotFoundException;
 
+public interface IPortfolio {
+  String getPortfolioComposition();
+
+  double getPortfolioValue();
+
+  void savePortfolio(String path) throws IllegalArgumentException;
+
+  void retrievePortfolio(String path) throws FileNotFoundException;
 }
