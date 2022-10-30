9d0932cf0a735455e9d538033753723c145ab8d6
add19 <aadish.deshpande@gmail.com>
2022-10-30 17:37:42 -0400

    Optimizer docs.

--- Stonks/src/portfolio/model/IStockAPIOptimizer.java
+++ Stonks/src/portfolio/model/IStockAPIOptimizer.java
@@ -4,6 +4,20 @@ package portfolio.model;
  * This interface represents a caching layer to avoid making additional API calls.
  */
 public interface IStockAPIOptimizer {
+
+  /**
+   * Gets the cached object for the specified key.
+   *
+   * @param tickerSymbol the key against which the object is to be fetched.
+   * @return the Stock object for the specified ticker symbol.
+   */
   IStock cacheGetObj(String tickerSymbol);
+
+  /**
+   * Sets the cached object for the specified key.
+   *
+   * @param tickerSymbol the key whose stock object is to be stored.
+   * @param stockObj the stock object to be stored.
+   */
   void cacheSetObj(String tickerSymbol, IStock stockObj);
 }
