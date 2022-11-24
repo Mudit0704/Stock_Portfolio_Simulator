66c07cd341bac28801826720bc687108afb55ec0
add19 <aadish.deshpande@gmail.com>
2022-11-24 11:51:40 -0500

    files cleaned up

--- Stonks/src/portfolio/model/StockReader.java
+++
@@ -1,9 +0,0 @@
-package portfolio.model;
-
-import java.time.LocalDate;
-
-public interface StockReader {
-  void readStockData(IStock stock, IStockService stockService);
-
-  LocalDate nextAvailableDate(LocalDate presentDate);
-}
